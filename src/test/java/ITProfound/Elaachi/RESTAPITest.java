03d45226a51ff643840212c04c887c64c6f3fff3
varshagurjar01 <gurjarvarsha71@gmail.com>
2021-05-03 17:57:23 +0530

    american developed

--- src/test/java/ITProfound/Elaachi/RESTAPITest.java
+++ src/test/java/ITProfound/Elaachi/RESTAPITest.java
@@ -7,6 +7,14 @@ public class RESTAPITest {
 	@Test
 	public void apiAutomation()
 	{
-		System.out.println("API automation");
+		System.out.println("API automation1");
+		System.out.println("API automation2");
+
+		System.out.println("API automation3");
+
+		System.out.println("API automation4");
+
+		System.out.println("API automation5");
+
 	}
 }
