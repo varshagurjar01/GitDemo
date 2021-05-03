90a2912b53a0dd3a2f06454a2d1e0674c7d7be88
varshagurjar01 <gurjarvarsha71@gmail.com>
2021-05-03 17:03:02 +0530

    this is commit from asian timezone guy

--- src/test/java/ITProfound/Elaachi/AppiumTest.java
+++ src/test/java/ITProfound/Elaachi/AppiumTest.java
@@ -8,5 +8,9 @@ public class AppiumTest {
 	public void appAutomation()
 	{
 		System.out.println("App automation");
+		System.out.println("automation 2");
+		System.out.println("automation 3");
+		System.out.println("automation 4");
+
 	}
 }
